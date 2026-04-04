fa8a3c08c13afaafaf008f452ec13dd1e34ca75a
chikuu <nanwithchikuu@gmail.com>
2026-04-05 02:53:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showFollow() {
+
+    let followBtn = document.getElementById("followBtn");
+
+    followBtn.classList.toggle("show");
+
+}
+function showonfollow() {
+    let amit = document.getElementById("amit");
+    amit.classList.toggle("showon");
+}
