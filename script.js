ad8f7cca01ef44d0f7d287748dac76e263878e7b
chikuu <nanwithchikuu@gmail.com>
2026-04-18 07:28:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+console.log('lets write javascript')
+
+
 function showFollow() {
 
     let followBtn = document.getElementById("followBtn");
@@ -9,3 +12,22 @@ function showonfollow() {
     let amit = document.getElementById("amit");
     amit.classList.toggle("showon");
 }
+async function main() {
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href)
+        }
+
+    }
+    console.log(songs)
+}
+
+main()
